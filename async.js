739ba81c4148319423d846668c46274b4dfa7e79
Abhishek <Abhisheksa782@gmail.com>
2024-01-26 16:07:33 +0530

    complete personal portfolio website

---
+++ async.js
@@ -0,0 +1,23 @@
+// // const target = document.querySelector('.container')
+// // target.addEventListener('click',()=>{
+// //   const element = document.createElement('p');
+// //   element.innerHTML = '<h1>Hello my love Vaishnavi</h1>';
+// //   target.append(element);
+// // })
+
+// const target = document.querySelector('.container')
+// let element = document.createElement('h1');
+// element.textContent = 'The timer is: '
+// target.appendChild(element)
+// let sec = 0;
+// target.textContent=`${sec}`
+// const timer = setInterval(()=>{
+//   target.textContent=`${sec}`
+//   sec++;
+//   if(sec>=10){
+//     clearInterval(timer)
+//   }
+// },1000)
+console.log('Hello');
+alert('Vaishnavi How are you?')
+console.log('Abhishek');
